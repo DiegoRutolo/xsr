1a8b0c748748ba9383fd837ceb7b2ddf9629f760
DiegoRutolo <diegorutolo@gmail.com>
2020-05-04 14:58:07 +0200

    Interpretar peticiones

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
-FROM maven:3-jdk-8-onbuild
-FROM java:8
+FROM maven:3-jdk-11
+FROM java:11
 EXPOSE 10097
 COPY --from=0 /usr/src/app/target/xsr-1.0-SNAPSHOT.jar /opt/xsr.jar
 CMD ["java", "-jar", "/opt/xsr.jar"]
--- src/main/java/eu/rutolo/xsr/server/Peticion.java
+++ src/main/java/eu/rutolo/xsr/server/Peticion.java
@@ -1,6 +1,11 @@
 package eu.rutolo.xsr.server;
 
+import java.io.BufferedReader;
+import java.util.function.Consumer;
+
 import org.json.JSONObject;
+
+import eu.rutolo.xsr.data.Log;
 
 public class Peticion {
 	// Codigos de tipo de petición
@@ -11,13 +16,86 @@ public class Peticion {
 
 	// Codigos de Apartado
 	public static final int X_CLIENTES = 5;
+	public static final int X_PEZAS = 6;
+	public static final int X_PEDIDOS = 7;
+	public static final int X_REPARACIONS = 8;
 
+	private String rawReq;
 	private int tipo;
 	private int apartado;
-	private int length;
 	private JSONObject content;
+	private JSONObject datos;
 
-	public Peticion(String reqStr) {
+	public Peticion(String rawReq) {
+		this.rawReq = rawReq;
+
+		this.content = new JSONObject(rawReq.substring(rawReq.indexOf("{")));
 		
+		// tipos
+		switch (content.getJSONObject("operacion").getString("tipo")) {
+			case "get":
+				this.tipo = Peticion.GET;
+				break;
+			case "create":
+				this.tipo = Peticion.CREATE;
+				break;
+			case "update":
+				this.tipo = Peticion.UPDATE;
+				break;
+			case "delete":
+				this.tipo = Peticion.DELETE;
+				break;
+			default:
+				this.tipo = 400;
+		}
+
+		// apartado
+		switch (content.getJSONObject("operacion").getString("apaartado")) {
+			case "x_clientes":
+				this.apartado = Peticion.X_CLIENTES;
+				break;
+			case "x_pezas":
+				this.apartado = Peticion.X_PEZAS;
+				break;
+			case "x_pedidos":
+				this.apartado = Peticion.X_PEDIDOS;
+				break;
+			case "x_reparacions":
+				this.apartado = Peticion.X_REPARACIONS;
+				break;
+			default:
+				this.apartado = 400;
+		}
+
+		// datos
+		this.datos = content.getJSONObject("operacion").getJSONObject("datos");
 	}
+	
+	//#region Getters
+	public String getRawReq() {
+		return rawReq;
+	}
+
+	public int getTipo() {
+		return tipo;
+	}
+
+	public int getApartado() {
+		return apartado;
+	}
+
+	public JSONObject getContent() {
+		return content;
+
+		/*
+		try {
+			return (JSONObject) content.clone();
+		} catch (CloneNotSupportedException e) {
+			Log.e("No se puede clonar???");
+			e.printStackTrace();
+			throw new RuntimeException("Inclonable");
+		}
+		*/	
+	}
+	//#endregion
 }
