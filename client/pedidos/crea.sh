5e8f8d8c75898a1443e85c730ea51d10d4462093
DiegoRutolo <diegorutolo@gmail.com>
2020-05-11 20:00:54 +0200

    Crear usa ID en vez de posicion

--- client/pedidos/crea.sh
+++ client/pedidos/crea.sh
@@ -3,18 +3,17 @@
 # Crea un pedido usando los argumentos. El primero es el cliente y el segundo es la pieza
 
 if [[ $# -lt 2 ]]; then
-	echo "Uso: crea.sh N_CLIENTE N_PIEZA [PRECIO] [ESTADO]"
+	echo "Uso: crea.sh ID_CLIENTE ID_PIEZA [PRECIO] [ESTADO]"
 	echo
-	echo "0 sería el primero, 1 el segundo, etc..."
 	exit
 fi
 
-if ! [[ $1 =~ ^[0-9]+$ ]]; then
+if ! [[ $1 =~ ^-*[0-9]+$ ]]; then
 	echo "Indica un número"
 	exit 1
 fi
 
-if ! [[ $2 =~ ^[0-9]+$ ]]; then
+if ! [[ $2 =~ ^-*[0-9]+$ ]]; then
 	echo "Indica un número"
 	exit 1
 fi
@@ -23,10 +22,6 @@ if ! [[ $3 =~ ^[0-9]+\.*[0-9]*$ ]]; then
 	echo "El precio debe ser un número"
 	exit 1
 fi
-
-ID_CLIENTE=$(../clientes/get.sh | jq -r ".data[$1].id")
-
-ID_PEZA=$(../pezas/get.sh | jq -r ".data[$2].id")
 
 curl -v -H "Content-Type: application/json" \
 	-d '{
@@ -38,10 +33,10 @@ curl -v -H "Content-Type: application/json" \
 			"tipo": "create",
 			"datos": {
 				"pedido": {
-					"idCliente": "'$ID_CLIENTE'",
-					"idPeza": "'$ID_PEZA'",
+					"idCliente": "'$1'",
+					"idPeza": "'$2'",
 					"pvp": "'$3'",
-					"estado": "'$4'"
+					"estado": "'"$4"'"
 				}
 			}
 		}
