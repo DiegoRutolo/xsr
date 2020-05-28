7ca9075a87aa819de2b98179e56d8d2dbad010b8
DiegoRutolo <diegorutolo@gmail.com>
2020-05-28 15:17:25 +0200

    Un par de ventanas

---
+++ pcxsr/README.md
@@ -0,0 +1,8 @@
+# PCXSR
+
+**Python client XSR** é un cliente para XSR
+
+## Dependencias
+
++ Python3
++ Tk
---
+++ pcxsr/pcxsr.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import os
+import tkinter as tk
+from PIL import ImageTk, Image
+from clases import clases
+
+basePath = os.path.dirname(__file__)
+resPath = os.path.join(basePath, "res")
+
+root = tk.Tk()
+root.title("PC-XSR")
+root.geometry("600x600")
+
+def abrirXclientes():
+	xc = clases.Xclientes(root, resPath)
+	xc.mainloop()
+
+img = ImageTk.PhotoImage(
+		Image.open(os.path.join(resPath, "customer.png")).resize((80, 80), Image.ANTIALIAS)
+	)
+
+tk.Button(root, text="Xestión clientes", image=img, command=abrirXclientes).pack()
+
+tk.mainloop()
